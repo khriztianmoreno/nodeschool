6838d40819121a23cf491defbf2c7a304c91a294
khriztianmoreno <khriztianmoreno@gmail.com>
2016-05-13 13:37:14 -0500

    Finish learyounode

--- learnyounode/fileServer.js
+++ learnyounode/fileServer.js
@@ -3,23 +3,22 @@ var http = require('http');
 
 var filename = process.argv[3];
 
-var server = http.createServer(function(request, response) {
+server = http.createServer(function(request, response) {
   fs.createReadStream(filename).pipe(response);
 });
-
-server.listen(Number(process.argv[2]);
+server.listen(process.argv[2]);
 
 /*
 
  var http = require('http')
     var fs = require('fs')
-    
+
     var server = http.createServer(function (req, res) {
       res.writeHead(200, { 'content-type': 'text/plain' })
-    
+
       fs.createReadStream(process.argv[3]).pipe(res)
     })
-    
+
     server.listen(Number(process.argv[2]))
 
 */
--- learnyounode/httpCollect.js
+++ learnyounode/httpCollect.js
@@ -4,26 +4,26 @@ var url = process.argv[2];
 
 http.get(url, function(response){
 	response.pipe(bl(function(err,data){
-		    if(err){
+		if(err){
 			console.log(err);
-		    };
-		    console.log(data.toString().length);
-		    console.log(data.toString());
-		}));
+		};
+		console.log(data.toString().length);
+		console.log(data.toString());
+	}));
 });
 
 /*
 var http = require('http')
-    var bl = require('bl')
-    
-    http.get(process.argv[2], function (response) {
-      response.pipe(bl(function (err, data) {
-        if (err)
-          return console.error(err)
-        data = data.toString()
-        console.log(data.length)
-        console.log(data)
-      }))  
-    });
+var bl = require('bl')
+
+http.get(process.argv[2], function (response) {
+	response.pipe(bl(function (err, data) {
+		if (err)
+			return console.error(err)
+		data = data.toString()
+		console.log(data.length)
+		console.log(data)
+	}))
+});
 
 */
